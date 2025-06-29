a8e4f92204c73656aac87503831553a26055157b
Rajesh Chhetri <rajeshthedeveloper2004@gmail.com>
2025-06-30 01:03:00 +0545

    Add comments and examples to 09_List.py for better understanding of lists in Python

--- 09_List.py
+++ 09_List.py
@@ -1,1 +1,51 @@
 # what is List
+# List भनेको Python मा multiple values राख्न मिल्ने container हो।
+
+# example 
+# fruits = ["apple", "banana", "mango"]
+# print(fruits)
+
+# # how to make a list 
+
+# my_list = [1, 2, 3, 4, 5]
+
+# my_list2 = ["ram","sita","geeta","hari"]
+
+# print(my_list)
+# print(my_list2)
+
+
+# # how to pickup word in List using indexing
+
+# name = ["sita","gita","ram"]
+
+# print(name[0])
+# print(name[2])
+
+
+# # how to add word in list 
+
+# names = ["Ram", "Sita"]
+# names.append("Hari")
+# print(names)
+# # Output: ["Ram", "Sita", "Hari"]
+
+# name = ["ravan","jesus"]
+# name.append("pray")
+# name.insert(1,"pray")
+# print(name)
+
+# # List ko bichama kura rakhana (insert)
+
+# name = ["ram","sita","hari"]
+# names.insert(1, "Gita")
+# print(names)
+# Output: ["Ram", "Gita", "Sita", "Hari"]
+
+name = ["raj","sam","har","sita"]
+name.append("den")
+name.insert(2,"dem")
+print(name)
+
+
+
